ece919ec5976c73c340de505cdbe6f5ca8c7090f
ShunsukeHayashi <ShunsukeHayashi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 04:40:15 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/notify-wp/trunk@1219655 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/plugin.class.php
@@ -0,0 +1,133 @@
+<?php
+
+class Notify_Plugin {
+	private $api;
+	private $page_hook = 'settings_page_notify-for-wordpress';
+
+	public $errors = null;
+	public $update = false;
+
+	public function __construct() {
+		add_action('admin_menu', array($this, 'register_menu_page'));
+		$this -> api = new Notify_API();
+		$this -> register_scripts();
+		$this -> register_hooks();
+
+		add_action('admin_init', array($this, 'http_requests'));
+	}
+
+	public function register_menu_page() {
+		add_options_page("notify", "notify", "manage_options", "notify-for-wordpress", array($this, 'load_page'));
+	}
+
+	public function publish_post_hook($strNewStatus, $strOldStatus, $post) {
+		if ((defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)) {
+			return;
+		}
+		$post_type = $post -> post_type;
+
+		if (($strOldStatus == 'draft' || $strOldStatus == 'auto-draft' || $strOldStatus == 'new') && $strNewStatus == 'publish') :
+			// New post/page published
+			$hooks = $this -> get_options();
+			$msg = $hooks -> notification_message;
+			$msg = str_replace("%title%", $post -> post_title, $msg);
+// 			$msg = str_replace("%url%", get_permalink($post -> ID), $msg);
+			$msg = str_replace("%url%", wp_get_shortlink($post -> ID), $msg);
+			$this -> api -> publish_post($msg);
+		endif;
+	}
+
+	public function register_hooks() {
+		$hooks = $this -> get_options();
+		if (is_object($hooks)) :
+			// Using same hook func for post and page actions.
+			add_action('transition_post_status', array($this, 'publish_post_hook'), 10, 3);
+		endif;
+	}
+
+	public function register_options($ops) {
+		if (is_array($ops))
+			$ops = json_encode($ops);
+		update_option('notify_options', $ops);
+	}
+
+	public function get_options() {
+		$ops = get_option('notify_options');
+		if (!$ops) {
+			add_option('notify_options', json_encode(array()));
+			$ops = get_option('notify_options');
+		}
+		$ops_decoded = json_decode($ops);
+		if (is_null($ops_decoded))
+			return $ops;
+		else
+			return $ops_decoded;
+	}
+
+	public function register_scripts() {
+		add_action('admin_print_styles-' . $this -> page_hook, array($this, 'notify_plugin_admin_styles'));
+	}
+
+	public function notify_plugin_admin_styles() {
+		wp_enqueue_style('notify-opensans-css', 'http://fonts.googleapis.com/css?family=Open+Sans:400,700,300');
+		wp_enqueue_style('notify-style-css', plugins_url('css/style.css', dirname(__FILE__)));
+	}
+
+	public function http_requests() {
+		if ($_GET["page"] == "notify-for-wordpress" && isset($_POST["notify_options_submit"])) {
+			if (!$this -> valid_notify_options_submit()) {
+				return;
+			}else{
+				$_POST["notification_embed"] = $this -> api -> get_embed($_POST["notification_token"]);
+				$this -> register_options($_POST);
+				$this -> update = true;
+			}
+		}
+	}
+
+	public function valid_notify_options_submit() {
+		if (empty($_POST["notification_message"])) {
+			$this -> errors["notification_message"] = "通知メッセージを入力してください";
+		}
+		if (empty($this -> errors)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static function make_request($url) {
+		if (function_exists('curl_version')) :
+			$CURL = curl_init();
+			curl_setopt($CURL, CURLOPT_URL, $url);
+			curl_setopt($CURL, CURLOPT_HEADER, 0);
+			curl_setopt($CURL, CURLOPT_RETURNTRANSFER, 1);
+			$data = curl_exec($CURL);
+			curl_close($CURL);
+			return $data;
+		else :
+			return file_get_contents($url);
+		endif;
+	}
+
+	public function getApi() {
+		return $this -> api;
+	}
+
+	public function getErrors() {
+		return $this -> errors;
+	}
+
+	public function getUpdate() {
+		return $this -> update;
+	}
+
+	public function getVersion() {
+		return "1.0.0";
+	}
+
+	public function load_page() {
+		require_once dirname(__FILE__) . '/../template/index.php';
+	}
+
+}
+?>
---
+++ template/index.php
@@ -0,0 +1,80 @@
+<?php
+$ops = $this -> get_options();
+$errors = $this -> getErrors();
+$update = $this -> getUpdate();
+$notification_message = @$ops->notification_message;
+if(empty($notification_message)){
+	$notification_message = "ブログを更新しました。 %title% %url%";
+}
+$notification_embed = @$ops->notification_embed;
+?>
+
+<div class="wrap">
+	<h2>
+		notify
+		<small> &nbsp;&nbsp;for WordPress &nbsp;&nbsp; version<?php echo $this->getVersion(); ?></small>
+	</h2>
+
+	<?php if(!empty($errors)){ ?>
+		<?php foreach ($errors as $key => $value) { ?>
+	<div id='<?php echo $key ?>' class='error settings-error'>
+		<p><strong><?php echo $value ?></strong></p>
+	</div>
+		<?php } ?>
+	<?php } ?>
+
+	<?php if($update){ ?>
+	<div id='setting-error-settings_updated' class='updated settings-error'>
+		<p><strong>設定を保存しました。</strong></p>
+	</div>
+	<?php } ?>
+
+	<p>
+		記事を公開した場合にnotifyから通知を送ることができます。
+	</p>
+
+	<form action="" method="POST" class="form-horizontal">
+		<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><label for="notification_token">通知のtoken</label></th>
+				<td>
+					<input name="notification_token" type="text" id="notification_token"
+					value="<?php echo @$ops->notification_token ?>" class="regular-text">
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label for="notification_message">通知メッセージ</label></th>
+				<td>
+					<textarea name="notification_message" id="app_notification_message"
+					 class="large-text code" rows="3"><?php echo $notification_message ?></textarea>
+					 
+					 <p class="description">変数は%%で囲んでください。使用できる変数はtitle(記事のタイトル) とurl(記事へのショートリンク)となっております。</p>
+				</td>
+			</tr>
+			
+		</tbody>
+		</table>
+		<p class="submit"><input type="submit" name="notify_options_submit" id="submit" class="button button-primary" value="変更を保存"></p>
+	</form>
+	
+	<table class="form-table">
+		<tbody>
+			<?php if(!empty($notification_embed)){ ?>
+			<tr valign="top">
+				<th scope="row"><label for="notification_message">埋め込みコード</label></th>
+				<td>
+					<textarea name="notification_embed" id="" class="large-text code" rows="7"><?php echo $notification_embed ?></textarea>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label for="notification_message">プレビュー</label></th>
+				<td>
+					<?php echo $notification_embed ?>
+				</td>
+			</tr>
+			<?php } ?>
+			</tbody>
+		</table>
+</div>
+
